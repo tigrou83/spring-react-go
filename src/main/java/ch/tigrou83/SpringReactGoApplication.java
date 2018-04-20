635f5659390fbad511c024dc24b134be3384bb24
Stéphane Schild <stephane.schild@gmail.com>
2018-04-20 10:22:22 +0200

    Initial commit

---
+++ src/main/java/ch/tigrou83/SpringReactGoApplication.java
@@ -0,0 +1,11 @@
+package ch.tigrou83;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactGoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactGoApplication.class, args);
+    }
+}
