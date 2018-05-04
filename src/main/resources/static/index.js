d8c9dea396ece2672ff59b21dbdbb022ed775e78
Stéphane Schild <stephane.schild@gmail.com>
2018-05-04 16:21:35 +0200

    index.js updated

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,46 +1,60 @@
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
+function Square(props) {
     return (
-      <button className="square" onClick={() => this.setState({value: 'O'})}>
-        {this.state.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			boardStatus: Array(19).fill(Array(19).fill(null)),
+			currentPlayer: 'X'
+		};
+	}
+	
+	handleClick(row, col) {
+		let currentValue = this.state.boardStatus[row][col];
+		if (currentValue === null) {
+			console.log('handleClick: ' + row + ' ' + col);
+			const boardStatusRows = this.state.boardStatus.slice();
+			const boardStatusCols = boardStatusRows[row].slice();
+			boardStatusCols[col] = this.state.currentPlayer;
+			boardStatusRows[row] = boardStatusCols;
+			this.setState({boardStatus: boardStatusRows});
+			// Change current player
+			let newCurrentPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X';
+			this.setState({currentPlayer: newCurrentPlayer});
+		}
+	}
+	
+	renderSquare(row, col) {
+		return <Square onClick={() => this.handleClick(row, col)} value={this.state.boardStatus[row][col]} />;
+	}
 
-  render() {
-    const status = 'Next player: X';
+	render() {
+		const status = 'Current player: ' + this.state.currentPlayer;
 
-    var nbOfRows = 19;
-    var nbOfColums = 19;
-    var rows = [];
-    for (var i=0;i<nbOfRows;i++) {
-      var columns = [];
-      for (var j=0;j<nbOfColums;j++) {
-        columns.push(this.renderSquare(j));
-      }
-      rows.push(<div className="board-row">{columns}</div>);
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        {rows}
-      </div>
-    );
-  }
+	    var nbOfRows = 19;
+	    var nbOfColums = 19;
+	    var rows = [];
+	    for (var i=0;i<nbOfRows;i++) {
+	      var columns = [];
+	      for (var j=0;j<nbOfColums;j++) {
+	        columns.push(this.renderSquare(i, j));
+	      }
+	      rows.push(<div className="board-row">{columns}</div>);
+	    }
+	
+	    return (
+	      <div>
+	        <div className="status">{status}</div>
+	        {rows}
+	      </div>
+	    );
+	}
 }
 
 class Game extends React.Component {
